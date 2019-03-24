8ba96831ccd304b47a04944c57668710119eb16d
Jing <jtan1993@gmail.com>
2019-03-24 08:34:42 +0000

    skyboxes

--- Custom Shaders/Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
+++ Custom Shaders/Assets/PostProcessing/Editor/PropertyDrawers/MinDrawer.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using UnityEngine.PostProcessing;
+using MinAttribute = UnityEngine.PostProcessing.MinAttribute;
 
 namespace UnityEditor.PostProcessing
-{
+{using MinAttribute = UnityEngine.PostProcessing.MinAttribute;
     [CustomPropertyDrawer(typeof(MinAttribute))]
     sealed class MinDrawer : PropertyDrawer
     {
--- Custom Shaders/Assets/Shaders/CelShadingForward.shader
+++ Custom Shaders/Assets/Shaders/CelShadingForward.shader
@@ -9,7 +9,9 @@
 		}
 		LOD 200
 
-		CGPROGRAM#pragma surface surf CelShadingForward#pragma target 3.0
+		CGPROGRAM
+		#pragma surface surf CelShadingForward
+		#pragma target 3.0
 
 		half4 LightingCelShadingForward(SurfaceOutput s, half3 lightDir, half atten) {
 			half NdotL = dot(s.Normal, lightDir);
